63a6ed818e777c30c365b276a10a2af7c198feed
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-26 17:34:53 +0800

    add pkg vue-i18n

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', 'vue-router', 'vue-i18n'],
       dts: 'src/auto-imports.d.ts'
     })
   ],
