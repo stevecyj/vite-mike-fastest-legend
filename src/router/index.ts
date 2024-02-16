663dc060110398d63a2c494083b5b1109adf9f47
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-16 10:54:04 +0800

    refactor: remove commented out code for importing HomeView
    - Remove commented out code for importing HomeView
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from '~pages'
-// import generatedRoutes from "virtual:generated-pages";
-// import HomeView from '../views/HomeView.vue'
 
 const routes = setupLayouts(generatedRoutes)
 
