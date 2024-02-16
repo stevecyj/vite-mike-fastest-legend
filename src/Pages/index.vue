44956b4954211e8e550917892ed7b6de150cc411
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-16 12:47:10 +0800

    feat: use store

--- src/Pages/index.vue
+++ src/Pages/index.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+const countStore = useCounterStore()
+const { name } = storeToRefs(countStore)
+const { setName } = countStore
+</script>
 
 <template>
   <main>
     <TheWelcome />
+    <!-- <h1>{{ countStore.name }}</h1> -->
+    <h1>{{ name }}</h1>
+    <!-- <button @click="countStore.setName">Click it</button> -->
+    <button @click="setName('Jotaro')">Click it</button>
   </main>
 </template>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,10 +3,15 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const name = ref('Dio')
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  const setName = (str: string) => {
+    name.value = str
+  }
+
+  return { count, name, doubleCount, increment, setName }
 })
